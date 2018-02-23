5374037e3677d250b8c8932792cc73a6ed4b10bf
Suz <soutsada@comcast.net>
2018-02-22 20:23:52 -0600

    Add files
    Add files

---
+++ app/suzwebsite/suz.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './suz.component.html',
+    styleUrls: ['./suz.component.css']
+})
+
+export class SuzComponent {
+    title = 'app';
+}
+
+
